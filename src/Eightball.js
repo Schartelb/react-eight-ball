8391b466f1da3eeba25eca0423bccc2bf153e14a
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-12-07 10:43:54 -0500

    Add files via upload

--- src/Eightball.js
+++ src/Eightball.js
@@ -5,25 +5,44 @@ import './eightball.css'
 const Eightball = () => {
     const [msg, setMsg] = useState("Think of a Question")
     const [color, setColor] = useState("black")
+    const [greenCount, setgreenCount] = useState(0)
+    const [yellowCount, setyellowCount] = useState(0)
+    const [redCount, setredCount] = useState(0)
     const askSpirits = () => {
         let pick = Math.floor(Math.random() * messages.length)
         setMsg(messages[pick].msg)
         setColor(messages[pick].color)
+        if (messages[pick].color == 'green') { setgreenCount(greenCount + 1) }
+        if (messages[pick].color == 'goldenrod') { setyellowCount(yellowCount + 1) }
+        if (messages[pick].color == 'red') { setredCount(redCount + 1) }
     }
     const reset = () => {
         setMsg("Think of a Question");
         setColor("black")
     }
     const status = color != "black"
+
+    /**Should put count in lower component but no, thank you. */
     const buttonact = () => {
-        status ? reset() : askSpirits()
+        status ? reset() : askSpirits();
+    }
+    const countReset = () => {
+        setgreenCount(0)
+        setyellowCount(0)
+        setredCount(0)
     }
 
     return (
-        <div className="circle">
-            <p className={color}>{msg}</p>
+        <>
+            <div className="circle">
+                <p className={color}>{msg}</p>
+            </div>
             <button onClick={buttonact}>{status ? "Reset" : "Ask the Spirits"}</button>
-        </div>
+
+            <p>Green: {greenCount}      Yellow: {yellowCount}       Red: {redCount}</p>
+            <button onClick={countReset}>Reset Count</button>
+
+        </>
     )
 }
 
